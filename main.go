b0da548a96379f80bc365b2c4752f66e41a321ec
“erto” <erikbt13@uia.no>
2023-01-12 16:05:24 +0100

    init commit

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+)
+
+// var outputDir = flag.String("o", "/js", " output directory name")
+
+func main() {
+	urls := make([]string, 0)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		urls = append(urls, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error reading from stdin: %v", err)
+	}
+	downloadUrls(urls)
+}
+
+func downloadUrls(urls []string) {
+	var wg sync.WaitGroup
+
+	wg.Add(len(urls))
+
+	for _, url := range urls {
+		go func(url string) {
+			defer wg.Done()
+			tokens := strings.Split(url, "/")
+			fileName := tokens[len(tokens)-1]
+			fmt.Println("Downloading", url, "to", fileName)
+
+			output, err := os.Create(fileName)
+			if err != nil {
+				log.Fatal("Error while creating", fileName, "-", err)
+			}
+			defer output.Close()
+
+			res, err := http.Get(url)
+			if err != nil {
+				log.Fatal("http get error: ", err)
+			} else {
+				defer res.Body.Close()
+				_, err = io.Copy(output, res.Body)
+				if err != nil {
+					log.Fatal("Error while downloading", url, "-", err)
+				} else {
+					fmt.Println("Downloaded", fileName)
+				}
+			}
+		}(url)
+	}
+	wg.Wait()
+	fmt.Println("Done")
+}
